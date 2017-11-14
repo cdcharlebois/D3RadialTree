2e47670064c805c18b057ca692b65e4f98aa0728
Russ Ferguson <russ.fergusson@adp.com>
2017-11-14 16:42:13 -0500

    Update to Zoom feature

--- src/DThreeRadialTree/widget/DThreeRadialTree.js
+++ src/DThreeRadialTree/widget/DThreeRadialTree.js
@@ -172,7 +172,10 @@ define([
             var treeSize = Math.floor((this.domNode.getBoundingClientRect().width / 3));
             var toolTipSize = '10px';
             var nodeSize = 16;
-            var selectedNode;
+            var active = d3.select(null);
+            var zoom = d3.zoom()
+                    .scaleExtent([1, 8])
+                    .on("zoom", zoomed);
 
             var width = this.domNode.getBoundingClientRect().width,
                 height = width;
@@ -188,9 +191,7 @@ define([
             var root = tree(stratify(__drawGraph));
 
             var svg = d3.select(this.domNode).append("svg").attr('width', width).attr('height', height)
-                .call(d3.zoom().on('zoom', function(){
-                    svg.attr("transform", d3.event.transform)
-                }))
+                svg.call(zoom);
             
 
 
@@ -220,7 +221,11 @@ define([
             feMerge.append("feMergeNode")
                 .attr("in", "SourceGraphic");
 
-            var g = svg.append("g").attr("transform", "translate(" + (width / 2) + "," + (height / 2 + 5) + ")");
+            var g = svg.append("g").attr("transform", "translate(" + (width / 2) + "," + (height / 2 + 5) + ")")
+
+            function zoomed() {
+                g.attr("transform", d3.event.transform); // updated for d3 v4
+              };
 
             var link = g.selectAll(".link")
                 .data(root.links())
